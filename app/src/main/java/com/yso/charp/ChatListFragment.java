0cf22fb5eff38a15b9ca24a8a3ec2b821413a6c1
Yossi Orlik <orliky@gmail.com>
2017-11-23 17:37:33 +0200

    sign in with phone num

--- app/src/main/java/com/yso/charp/ChatListFragment.java
+++ app/src/main/java/com/yso/charp/ChatListFragment.java
@@ -2,6 +2,7 @@ package com.yso.charp;
 
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,14 +12,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 
 /**
--- app/src/main/java/com/yso/charp/MainActivity.java
+++ app/src/main/java/com/yso/charp/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
@@ -18,24 +17,14 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity
 {
     private static String TAG = MainActivity.class.getSimpleName();
 
     private static int SIGN_IN_REQUEST_CODE = 9981;
-//    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
-    private String mVerificationId;
-//    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
 
     @Override
@@ -44,82 +33,26 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-//                "0525610118",        // Phone number to verify
-//                60,                 // Timeout duration
-//                TimeUnit.SECONDS,   // Unit of timeout
-//                this,               // Activity (for callback binding)
-//                mCallbacks);        // OnVerificationStateChangedCallbacks
-//
-//        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-//
-//            @Override
-//            public void onVerificationCompleted(PhoneAuthCredential credential) {
-//                // This callback will be invoked in two situations:
-//                // 1 - Instant verification. In some cases the phone number can be instantly
-//                //     verified without needing to send or enter a verification code.
-//                // 2 - Auto-retrieval. On some devices Google Play services can automatically
-//                //     detect the incoming verification SMS and perform verificaiton without
-//                //     user action.
-//                Log.d(TAG, "onVerificationCompleted:" + credential);
-//
-//                signInWithPhoneAuthCredential(credential);
-//            }
-//
-//            @Override
-//            public void onVerificationFailed(FirebaseException e) {
-//                // This callback is invoked in an invalid request for verification is made,
-//                // for instance if the the phone number format is not valid.
-//                Log.w(TAG, "onVerificationFailed", e);
-//
-//                if (e instanceof FirebaseAuthInvalidCredentialsException) {
-//                    // Invalid request
-//                    // ...
-//                } else if (e instanceof FirebaseTooManyRequestsException) {
-//                    // The SMS quota for the project has been exceeded
-//                    // ...
-//                }
-//
-//                // Show a message and update the UI
-//                // ...
-//            }
-//
-//            @Override
-//            public void onCodeSent(String verificationId,
-//                                   PhoneAuthProvider.ForceResendingToken token) {
-//                // The SMS verification code has been sent to the provided phone number, we
-//                // now need to ask the user to enter the code and then construct a credential
-//                // by combining the code with a verification ID.
-//                Log.d(TAG, "onCodeSent:" + verificationId);
-//
-//                // Save verification ID and resending token so we can use them later
-//                mVerificationId = verificationId;
-//                mResendToken = token;
-//
-//                // ...
-//            }
-//        };
-
-
-        if (FirebaseAuth.getInstance().getCurrentUser() == null)
+        if (FirebaseAuth.getInstance().getCurrentUser() == null || FirebaseAuth.getInstance().getCurrentUser().getDisplayName().equals(""))
         {
             // Start sign in/sign up activity
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+//            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+            startActivity(new Intent(this, PhoneAuthActivity.class));
+            finish();
         }
         else
         {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
             // User is already signed in. Therefore, display
             // a welcome Toast
             Toast.makeText(this, "Welcome " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
 
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new ChatListFragment(ChatListFragment.TypeList.CHATS)).commit();
-            //            chatID = FirebaseDatabase.getInstance().getReference().child().push().getKey();
 
 
-            /*String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            /*//            chatID = FirebaseDatabase.getInstance().getReference().child().push().getKey();
+            String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
             DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
             Query query = rootRef.child(FirebaseAuth.getInstance().getCurrentUser().getDisplayName()).child(chatID).orderByChild(UID).equalTo(false);
             query.addChildEventListener(new ChildEventListener()
@@ -167,28 +100,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-//    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-//        FirebaseAuth.getInstance().signInWithCredential(credential)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "signInWithCredential:success");
-//
-//                            FirebaseUser user = task.getResult().getUser();
-//                            // ...
-//                        } else {
-//                            // Sign in failed, display a message and update the UI
-//                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-//                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-//                                // The verification code entered was invalid
-//                            }
-//                        }
-//                    }
-//                });
-//    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -217,7 +128,6 @@ public class MainActivity extends AppCompatActivity
         else if (item.getItemId() == R.id.menu_user_list)
         {
             getSupportFragmentManager().beginTransaction().replace(R.id.container, new ChatListFragment(ChatListFragment.TypeList.USERS)).commit();
-//            getSupportFragmentManager().beginTransaction().replace(R.id.container, new UserListFragment()).commit();
         }
         else if (item.getItemId() == R.id.menu_chats_list)
         {
@@ -246,7 +156,6 @@ public class MainActivity extends AppCompatActivity
             {
                 Toast.makeText(this, "We couldn't sign you in. Please try again later.", Toast.LENGTH_LONG).show();
 
-                // Close the app
                 finish();
             }
         }
